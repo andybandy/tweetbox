d30e330a19324266ad6d6bf30ede659c90e17cba
Andrei Shchatko <andrei.shchatko@gmail.com>
2016-07-10 14:53:04 +0300

    Adds text to TweetBox component state.

--- src/app.js
+++ src/app.js
@@ -2,12 +2,28 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 class TweetBox extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      text: ''
+    };
+  };
+
+  handleChange(event) {
+    this.setState({ text: event.target.value });
+  };
+
   render() {
     return (
       <div className="well clearfix">
-        <textarea className="form-control"></textarea>
+        <textarea className="form-control"
+                  onChange={this.handleChange}>
+        </textarea>
         <br/>
-        <button className="btn btn-primary pull-right">twt</button>
+        <button className="btn btn-primary pull-right"
+                disabled={this.state.text.length === 0}>twt</button>
       </div>
     )
   }
