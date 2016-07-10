43628ffc629015c8240db883658ed6b80161fce2
Andrei Shchatko <andrei.shchatko@gmail.com>
2016-07-10 15:00:02 +0300

    Fixes character counter.

--- src/app.js
+++ src/app.js
@@ -21,7 +21,8 @@ class TweetBox extends React.Component {
         <textarea className="form-control"
                   onChange={this.handleChange}>
         </textarea>
-        <span>{this.state.text.length}</span>
+        <br/>
+        <span>{140 - this.state.text.length}</span>
         <button className="btn btn-primary pull-right"
                 disabled={this.state.text.length === 0}>twt</button>
       </div>
